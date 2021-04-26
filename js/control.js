c49e136195a73f8679c8f480fee92cee7097d96c
frsdjefaflia <djefaflia.fares@gmail.com>
2021-04-26 12:10:51 +0100

    add control js and fix responsive

---
+++ js/control.js
@@ -0,0 +1,7 @@
+//ADD YOUR SOCIAL MEDIA
+facebook = "";
+instagram = "";
+twitter = "";
+// YOUR NAME AND LINK FOR GOOGLE PLAY
+yourName ="your google play";
+yourLink = "";
---
+++ js/script.js
@@ -0,0 +1,15 @@
+//FOR SOCIAL MEDIA -HEADER AND FOOTER
+var social, facebook, instagram, twitter;
+    social = document.getElementsByClassName("social");
+    social[0].href = facebook;
+    social[1].href = instagram;
+    social[2].href = twitter;
+    social[3].href = facebook;
+    social[4].href = instagram;
+    social[5].href = twitter;
+//FOR NAME AND LINK IN GOOGLE PLAY
+var googlePlay, yourName, yourLink;
+    googlePlay = document.getElementsByClassName("googlePlay");
+    googlePlay[0].innerHTML = yourName;
+    googlePlay[0].href = yourLink;
+    googlePlay[1].href = yourLink;
