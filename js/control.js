73be805b4fc8c1054a464a9990af1d0809a72256
frsdjefaflia <djefaflia.fares@gmail.com>
2021-04-27 15:11:01 +0100

    add js for spcl app

--- js/control.js
+++ js/control.js
@@ -1,7 +1,57 @@
-//ADD YOUR SOCIAL MEDIA
+/************** 01 - ADD YOUR SOCIAL MEDIA *************/
+
+//Just paste the link for your account between the quotation marks.
 facebook = "";
+
+//Just paste the link for your account between the quotation marks.
 instagram = "";
+
+//Just paste the link for your account between the quotation marks.
 twitter = "";
-// YOUR NAME AND LINK FOR GOOGLE PLAY
-yourName ="your google play";
+/************** END *************/
+
+
+
+/************** 02 - YOUR NAME AND LINK FOR GOOGLE PLAY *************/
+
+//yourName: Enter the name of your account or page in Google Play between the quotation marks, to hide leave the value blank.
+yourName ="your google play"; 
+
+//yourLink: Copy the link for your page in Google Play and place it in the quotation marks
 yourLink = "";
+/************** END *************/
+
+
+
+/************** 03 - THE TITLE *************/
+
+//In order to move to the beginning of the line we are using "<br>".
+title01 = "Choose your app" + "<br>" + "now";
+title02 = "More" + "<br>" + "applications";
+/************** END *************/
+
+
+
+/************** 04 - FOR FEATURED APP *************/
+
+//First of all, you should put all the pictures of your applications inside a folder: "img"
+//In order to modify the image, copy its path from img folder: "img -> name folder" "yourimage.jpg -> your img"
+appImg = "img/android.jpg"; 
+
+//Modify the name or title of the application from here.
+appName = "Lorem ipsum dolor sit amet"; 
+
+//Status of the application, you can write the price of the application or write "free" if the application is really free.
+appStatus = "free"; 
+
+//Medium description, clarifying the mission of your application.
+appDescription = "Lorem ipsum dolor sit amet," 
+                + "consectetur adipiscing elit." 
+                + "Proin sit amet erat eget eros placerat ullamcorper in sit amet dui."; 
+
+//Name the button, maybe "download" if it's free or "buy" if it's paid.
+appButtoun = "download"; 
+
+//The link for the application from the Google Play Store.
+appLink = "#";
+/************** END *************/
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//FOR SOCIAL MEDIA -HEADER AND FOOTER
+//FOR SOCIAL MEDIA - HEADER AND FOOTER
 var social, facebook, instagram, twitter;
     social = document.getElementsByClassName("social");
     social[0].href = facebook;
@@ -7,9 +7,28 @@ var social, facebook, instagram, twitter;
     social[3].href = facebook;
     social[4].href = instagram;
     social[5].href = twitter;
+
 //FOR NAME AND LINK IN GOOGLE PLAY
-var googlePlay, yourName, yourLink;
+var googlePlay, yourName, yourLink, googlePlay_Display;
     googlePlay = document.getElementsByClassName("googlePlay");
     googlePlay[0].innerHTML = yourName;
     googlePlay[0].href = yourLink;
     googlePlay[1].href = yourLink;
+
+
+//FOR TITLE
+var mainTitle, title01, title02;
+    mainTitle = document.getElementsByClassName("h1-title");
+    mainTitle[0].innerHTML = title01;
+    mainTitle[1].innerHTML = title02;
+
+
+//for Featured application
+var nameAppFeatured, appImg, appName, appStatus, appDescription, appButtoun, appLink;
+    nameAppFeatured = document.getElementsByClassName("spclApp");
+    nameAppFeatured[0].src = appImg;
+    nameAppFeatured[1].innerHTML = appName;
+    nameAppFeatured[2].innerHTML = appStatus;
+    nameAppFeatured[3].innerHTML = appDescription;
+    nameAppFeatured[4].innerHTML = appButtoun;
+    nameAppFeatured[4].href = appLink;
