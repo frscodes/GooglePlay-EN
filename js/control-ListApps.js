354618c688816e2eb70e2321e2d1abc79e208889
frsdjefaflia <djefaflia.fares@gmail.com>
2021-05-31 21:36:19 +0100

    Development in the control panel

---
+++ js/control-ListApps.js
@@ -0,0 +1,19 @@
+/************** 05 - FOR EDIT APPS *************/
+//First of all, you should put all the pictures of your applications inside a folder: "img"
+//HOW WORK THIS CODE: ImgApp[number the app].src = "your value"
+
+//for edit the application image:img/Full-name-of-the-image.jpg/png
+ImgApp[0].src = "img/android.jpg"
+
+//for edit the application name:
+TitleApp[0].textContent = "FrsCodes"
+
+//status the app: free or paid
+StatusApp[0].textContent = "free"
+
+//name the button: download or buy
+ButtonApp[0].textContent = "download"
+
+//link your app
+ButtonApp[0].href = "#"
+/************** END *************/
--- js/script.js
+++ js/script.js
@@ -35,214 +35,41 @@ var nameAppFeatured, appImg, appName, appStatus, appDescription, appButtoun, app
 
 
 //FOR EDIT APPS LIST
-//ITEM01
-var editAPP01, app01, imgApp01, nameApp01, statusApp01, btnApp01, linkApp01;
-    editAPP01 = document.getElementsByClassName("editApp01");
-    if (app01 === "show"){
-        document.getElementById("editApp01").style.display = "block";
-    }
-    else if (app01 === "hide"){
-        document.getElementById("editApp01").style.display = "none";
-    };
-    editAPP01[0].src = imgApp01;
-    editAPP01[1].innerHTML = nameApp01;
-    editAPP01[2].innerHTML = statusApp01;
-    editAPP01[3].innerHTML = btnApp01;
-    editAPP01[3].href = linkApp01;
 
-//ITEM02
-var editAPP02, app02, imgApp02, nameApp02, statusApp02, btnApp02, linkApp02;
-    editAPP02 = document.getElementsByClassName("editApp02");
-    if (app02 === "show"){
-        document.getElementById("editApp02").style.display = "block";
-    }
-    else if (app02 === "hide"){
-        document.getElementById("editApp02").style.display = "none";
-    };
-    editAPP02[0].src = imgApp02;
-    editAPP02[1].innerHTML = nameApp02;
-    editAPP02[2].innerHTML = statusApp02;
-    editAPP02[3].innerHTML = btnApp02;
-    editAPP02[3].href = linkApp02;
+var apps, mainListapps;
 
-
-//ITEM03
-var editAPP03, app03, imgApp03, nameApp03, statusApp03, btnApp03, linkApp03;
-    editAPP03 = document.getElementsByClassName("editApp03");
-    if (app03 === "show"){
-        document.getElementById("editApp03").style.display = "block";
-    }
-    else if (app03 === "hide"){
-        document.getElementById("editApp03").style.display = "none";
-    };
-    editAPP03[0].src = imgApp03;
-    editAPP03[1].innerHTML = nameApp03;
-    editAPP03[2].innerHTML = statusApp03;
-    editAPP03[3].innerHTML = btnApp03;
-    editAPP03[3].href = linkApp03;
-
-
-//ITEM04
-var editAPP04, app04, imgApp04, nameApp04, statusApp04, btnApp04, linkApp04;
-    editAPP04 = document.getElementsByClassName("editApp04");
-    if (app04 === "show"){
-        document.getElementById("editApp04").style.display = "block";
-    }
-    else if (app04 === "hide"){
-        document.getElementById("editApp04").style.display = "none";
-    };
-    editAPP04[0].src = imgApp04;
-    editAPP04[1].innerHTML = nameApp04;
-    editAPP04[2].innerHTML = statusApp04;
-    editAPP04[3].innerHTML = btnApp04;
-    editAPP04[3].href = linkApp04;
-
-
-//ITEM05
-var editAPP05, app05, imgApp05, nameApp05, statusApp05, btnApp05, linkApp05;
-    editAPP05 = document.getElementsByClassName("editApp05");
-    if (app05 === "show"){
-        document.getElementById("editApp05").style.display = "block";
-    }
-    else if (app05 === "hide"){
-        document.getElementById("editApp05").style.display = "none";
-    };
-    editAPP05[0].src = imgApp05;
-    editAPP05[1].innerHTML = nameApp05;
-    editAPP05[2].innerHTML = statusApp05;
-    editAPP05[3].innerHTML = btnApp05;
-    editAPP05[3].href = linkApp05;
-
-
-//ITEM06
-var editAPP06, app06, imgApp06, nameApp06, statusApp06, btnApp06, linkApp06;
-    editAPP06 = document.getElementsByClassName("editApp06");
-    if (app06 === "show"){
-        document.getElementById("editApp06").style.display = "block";
-    }
-    else if (app06 === "hide"){
-        document.getElementById("editApp06").style.display = "none";
-    };
-    editAPP06[0].src = imgApp06;
-    editAPP06[1].innerHTML = nameApp06;
-    editAPP06[2].innerHTML = statusApp06;
-    editAPP06[3].innerHTML = btnApp06;
-    editAPP06[3].href = linkApp06;
-
-
-//ITEM07
-var editAPP07, app07, imgApp07, nameApp07, statusApp07, btnApp07, linkApp07;
-    editAPP07 = document.getElementsByClassName("editApp07");
-    if (app07 === "show"){
-        document.getElementById("editApp07").style.display = "block";
-    }
-    else if (app07 === "hide"){
-        document.getElementById("editApp07").style.display = "none";
-    };
-    editAPP07[0].src = imgApp07;
-    editAPP07[1].innerHTML = nameApp07;
-    editAPP07[2].innerHTML = statusApp07;
-    editAPP07[3].innerHTML = btnApp07;
-    editAPP07[3].href = linkApp07;
-
-
-//ITEM08
-var editAPP08, app08, imgApp08, nameApp08, statusApp08, btnApp08, linkApp08;
-    editAPP08 = document.getElementsByClassName("editApp08");
-    if (app08 === "show"){
-        document.getElementById("editApp08").style.display = "block";
-    }
-    else if (app08 === "hide"){
-        document.getElementById("editApp08").style.display = "none";
-    };
-    editAPP08[0].src = imgApp08;
-    editAPP08[1].innerHTML = nameApp08;
-    editAPP08[2].innerHTML = statusApp08;
-    editAPP08[3].innerHTML = btnApp08;
-    editAPP08[3].href = linkApp08;
-
-
-//ITEM09
-var editAPP09, app09, imgApp09, nameApp09, statusApp09, btnApp09, linkApp09;
-    editAPP09 = document.getElementsByClassName("editApp09");
-    if (app09 === "show"){
-        document.getElementById("editApp09").style.display = "block";
-    }
-    else if (app09 === "hide"){
-        document.getElementById("editApp09").style.display = "none";
-    };
-    editAPP09[0].src = imgApp09;
-    editAPP09[1].innerHTML = nameApp09;
-    editAPP09[2].innerHTML = statusApp09;
-    editAPP09[3].innerHTML = btnApp09;
-    editAPP09[3].href = linkApp09;
-
-
-//ITEM10
-var editAPP10, app10, imgApp10, nameApp10, statusApp10, btnApp10, linkApp10;
-    editAPP10 = document.getElementsByClassName("editApp10");
-    if (app10 === "show"){
-        document.getElementById("editApp10").style.display = "block";
-    }
-    else if (app10 === "hide"){
-        document.getElementById("editApp10").style.display = "none";
-    };
-    editAPP10[0].src = imgApp10;
-    editAPP10[1].innerHTML = nameApp10;
-    editAPP10[2].innerHTML = statusApp10;
-    editAPP10[3].innerHTML = btnApp10;
-    editAPP10[3].href = linkApp10;
-
-
-//ITEM11
-var editAPP11, app11, imgApp11, nameApp11, statusApp11, btnApp11, linkApp11;
-    editAPP11 = document.getElementsByClassName("editApp11");
-    if (app11 === "show"){
-        document.getElementById("editApp11").style.display = "block";
-    }
-    else if (app11 === "hide"){
-        document.getElementById("editApp11").style.display = "none";
-    };
-    editAPP11[0].src = imgApp11;
-    editAPP11[1].innerHTML = nameApp11;
-    editAPP11[2].innerHTML = statusApp11;
-    editAPP11[3].innerHTML = btnApp11;
-    editAPP11[3].href = linkApp11;
-
-
-//ITEM12
-var editAPP12, app12, imgApp12, nameApp12, statusApp12, btnApp12, linkApp12;
-    editAPP12 = document.getElementsByClassName("editApp12");
-    if (app12 === "show"){
-        document.getElementById("editApp12").style.display = "block";
-    }
-    else if (app12 === "hide"){
-        document.getElementById("editApp12").style.display = "none";
-    };
-    editAPP12[0].src = imgApp12;
-    editAPP12[1].innerHTML = nameApp12;
-    editAPP12[2].innerHTML = statusApp12;
-    editAPP12[3].innerHTML = btnApp12;
-    editAPP12[3].href = linkApp12;
-
-/*
-//ITEMXX
-var editAPPXX, appXX, imgAppXX, nameAppXX, statusAppXX, btnAppXX, linkAppXX;
-    editAPPXX = document.getElementsByClassName("editAppXX");
-    if (appXX === "show"){
-        document.getElementById("editAppXX").style.display = "block";
-    }
-    else if (appXX === "hide"){
-        document.getElementById("editAppXX").style.display = "none";
-    };
-    editAPPXX[0].src = imgAppXX;
-    editAPPXX[1].innerHTML = nameAppXX;
-    editAPPXX[2].innerHTML = statusAppXX;
-    editAPPXX[3].innerHTML = btnAppXX;
-    editAPPXX[3].href = linkAppXX;
-*/
-
-
+    mainListapps = document.querySelector('.main-listapps');
+for(i = 1; i <= apps; i++){
+    var article = document.createElement('article');
+        mainListapps.appendChild(article)
+    var editApp = document.createElement('div');
+        article.appendChild(editApp);
+        editApp.setAttribute('class', 'listapps-cntr');
+    var listappsImg = document.createElement('div');
+        editApp.appendChild(listappsImg);
+        listappsImg.setAttribute('class', 'listapps-img');
+    var imgapp = document.createElement('img');
+        listappsImg.appendChild(imgapp);
+        imgapp.setAttribute('class', 'my-img');
+        imgapp.src = "";
+    var listappsInfo = document.createElement('div');
+        editApp.appendChild(listappsInfo);
+        listappsInfo.setAttribute('class', 'listapps-info');
+    var h4 = document.createElement('h4');
+        h4.setAttribute('class', 'my-h4');
+    var span = document.createElement('span');
+        span.setAttribute('class', 'my-span');
+    var a = document.createElement('a');
+        a.setAttribute('class', 'my-a');
+        listappsInfo.appendChild(h4);
+        listappsInfo.appendChild(span);
+        listappsInfo.appendChild(a);
+    
+};
+var ImgApp = document.getElementsByClassName('my-img');
+var TitleApp = document.getElementsByClassName('my-h4');
+var StatusApp = document.getElementsByClassName('my-span');
+var ButtonApp = document.getElementsByClassName('my-a');
 //FOR COPYRIGHT 
 var copyright, yourCopyright;
     copyright = document.getElementsByClassName("copyright");
